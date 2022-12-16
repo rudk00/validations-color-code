291980d4b0aca00f3d3d39b1bc94328dea64aabd
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-12-16 14:06:07 +0300

    Color validation success

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -5,9 +5,16 @@ import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
-
-        // Put your code here
-
+        if (color == null||color == "\\s+") return false;
+        Pattern pattern6 = Pattern.compile("#[0-9a-fA-F]{6}");
+        Pattern pattern3 = Pattern.compile("#[0-9a-fA-F]{3}");
+        Matcher matcher6 = pattern6.matcher(color);
+        Matcher matcher3 = pattern3.matcher(color);
+        if (color.length() <=4){
+            return (matcher3.matches());
+        } else {
+            return (matcher6.matches());
+        }
     }
 }
 
